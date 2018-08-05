4cb1d3913e56f0fd325e1c627bb52cb4a746f40c
Becca Lasky <becca@Aviads-MacBook-Air.local>
2018-08-05 16:40:46 -0700

    checkpoint 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,19 @@
+window.onload = function() {
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+               animatePoints(pointsArray);
+         }
+       });
+};
+var pointsArray = document.getElementsByClassName('point');
 
-function animatePoints(){
-
-    var points = document.getElementsByClassName('point');
-
+function animatePoints(points){
 
     function revealPoint(indexNum){
       points[indexNum].style.opacity = 1;
@@ -15,15 +25,12 @@ function animatePoints(){
     for (var i = 0; i < points.length; i++){
       revealPoint(i);
       }
-
-
 }
-
-window.onload = function() {
-  setTimeout(loadAfterTime, 250)
-};
+animatePoints();
 
 
+/*
 function loadAfterTime(){
 animatePoints();
 }
+*/
