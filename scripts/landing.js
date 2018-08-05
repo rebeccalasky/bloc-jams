957b0d549e6ddf655acd6c111ff15296ba7b4140
Becca Lasky <becca@Aviads-MacBook-Air.local>
2018-08-05 15:03:24 -0700

    assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,25 @@
-var animatePoints = function() {
+
+
+function loadAfterTime(){
+
 
     var points = document.getElementsByClassName('point');
 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    function revealPoint(indexNum){
+      points[indexNum].style.opacity = 1;
+      points[indexNum].style.transform = "scaleX(1) translateY(0)";
+      points[indexNum].style.msTransform = "scaleX(1) translateY(0)";
+      points[indexNum].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    for (var i = 0; i < points.length; i++){
+      revealPoint(i);
+      }
 
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
 
 };
 
-animatePoints();
+window.onload = function() {
+  setTimeout(loadAfterTime, 250)
+};
